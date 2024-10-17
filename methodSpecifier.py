63f73b3174ac6f34dba22dc00f91917393f9dc54
Raven <RavenHawks@gmail.com>
2024-10-17 21:13:09 +0800

    New Version

--- methodSpecifier.py
+++ methodSpecifier.py
@@ -6,8 +6,5 @@ class pingInvoker:
     def __init__(self) -> None:
         networkFunction: main.webPockets = main.webPockets(200, 100)
         networkFunction.webster_message()
-        print("")
-        print(f"Delta Team has joined the server.")
-        print(f'Alpha Team has joined the server.')
 
 pingInject: pingInvoker = pingInvoker()
